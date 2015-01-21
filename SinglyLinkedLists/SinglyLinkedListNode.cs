eb546490ec85c9e5d61e9041bcaa44f9399d1f79
gregkorte <gryphionsroost@yahoo.com>
2015-01-21 11:39:04 -0600

    Fifteen tests passing.

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -53,7 +53,17 @@ namespace SinglyLinkedLists
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
         public int CompareTo(Object obj)
         {
-            throw new NotImplementedException();
+            int first = 0;
+            foreach (char c in this.value)
+            {
+                first += (int)c;
+            }
+            int second = 0;
+            foreach (char c in obj.ToString())
+            {
+                second += (int)c;
+            }
+            return first - second;
         }
 
         public bool IsLast()
@@ -72,11 +82,14 @@ namespace SinglyLinkedLists
 
         public override bool Equals(Object obj)
         {
-            if (this.value == obj.ToString())
-            {
-                return true;
+            if (this.GetType() == obj.GetType()){
+
+                if (this.value == obj.ToString())
+                {
+                    return true;
+                }
             }
             return false;
-        }
+            }
     }
 }
